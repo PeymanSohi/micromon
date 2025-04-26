75cfaac000f17b360b94ed056a19bfc5fd1a0e5b
Peyman_Sohi <peymantahmasebi10@gmail.com>
2025-04-26 15:47:20 +0330

    fix name

--- frontend/app.js
+++
@@ -1,9 +0,0 @@
-fetch('/api')
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('api-response').innerText = data.message;
-  })
-  .catch(error => {
-    document.getElementById('api-response').innerText = 'Error fetching API.';
-  });
-
